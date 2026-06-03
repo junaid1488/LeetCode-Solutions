e3dc5e8e29f77b6a249642699eec5f42a55a63cb
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-03 15:14:09 +0530

    Implement function to calculate minimum cost of arrays

---
+++ Arrays/3424_minimum_cost_to_make_arrays_identical.cpp
@@ -0,0 +1,12 @@
+    long long minCost(vector<int>& a, vector<int>& b, long long k) {
+        long long res1 = 0, res2 = 0, n = a.size();
+        for (int i = 0; i < n; ++i) {
+            res1 += abs(a[i] - b[i]);
+        }
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+        for (int i = 0; i < n; ++i) {
+            res2 += abs(a[i] - b[i]);
+        }
+        return min(res1, res2 + k);
+    }
