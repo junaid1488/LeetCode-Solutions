bbc7035d015b92fc7987943a3b384003a579aff9
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-06 19:20:57 +0530

    Create 83-remove-duplicates-from-sorted-list.cpp

---
+++ Linked Lists/83-remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+
+        ListNode* curr = head;
+
+        while (curr && curr->next) {
+
+            // Duplicate found
+            if (curr->val == curr->next->val) {
+                curr->next = curr->next->next;
+            }
+            else {
+                curr = curr->next;
+            }
+        }
+
+        return head;
+    }
+};
