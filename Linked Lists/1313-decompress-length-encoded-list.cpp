937f71755e95e1730e516dab60be39f46de8a186
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-06 19:23:57 +0530

    Create 1313-decompress-length-encoded-list.cpp

---
+++ Linked Lists/1313-decompress-length-encoded-list.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i += 2) {
+
+            int freq = nums[i];
+            int val = nums[i + 1];
+
+            // Add val freq times
+            while (freq--) {
+                ans.push_back(val);
+            }
+        }
+
+        return ans;
+    }
+};
