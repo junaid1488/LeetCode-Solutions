81d98979f19d1ce7e579a503d0757ff852799b26
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-08 17:26:24 +0530

    Create 51-N-Queen.cpp

---
+++ Queue/51-N-Queen.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+
+    void solve(int row, int n,
+               vector<string>& board,
+               unordered_set<int>& cols,
+               unordered_set<int>& diag1,
+               unordered_set<int>& diag2) {
+
+        if (row == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+
+            if (cols.count(col) ||
+                diag1.count(row - col) ||
+                diag2.count(row + col))
+                continue;
+
+            board[row][col] = 'Q';
+
+            cols.insert(col);
+            diag1.insert(row - col);
+            diag2.insert(row + col);
+
+            solve(row + 1, n, board, cols, diag1, diag2);
+
+            board[row][col] = '.';
+
+            cols.erase(col);
+            diag1.erase(row - col);
+            diag2.erase(row + col);
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+
+        vector<string> board(n, string(n, '.'));
+
+        unordered_set<int> cols;
+        unordered_set<int> diag1;
+        unordered_set<int> diag2;
+
+        solve(0, n, board, cols, diag1, diag2);
+
+        return ans;
+    }
+};
