ce76b042c5a726009cc546ac724851655e10bd30
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-12 15:19:00 +0530

    Create 1204-last-person-to-fit-in-the-bus.sql

---
+++ SQL/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,13 @@
+WITH cte AS (
+    SELECT
+        person_name,
+        weight,
+        turn,
+        SUM(weight) OVER(ORDER BY turn) AS total_weight
+    FROM Queue
+)
+SELECT person_name
+FROM cte
+WHERE total_weight <= 1000
+ORDER BY turn DESC
+LIMIT 1;
