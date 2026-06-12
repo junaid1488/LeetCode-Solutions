5ecabcdd521b229cb3c86ac33eea6b9ed459c499
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-12 15:12:27 +0530

    Create 180-consecutive-numbers.sql

---
+++ SQL/180-consecutive-numbers.sql
@@ -0,0 +1,10 @@
+SELECT DISTINCT Num AS ConsecutiveNums
+FROM (
+    SELECT
+        Num,
+        LAG(Num, 1) OVER (ORDER BY id) AS prev1,
+        LAG(Num, 2) OVER (ORDER BY id) AS prev2
+    FROM Logs
+) t
+WHERE Num = prev1
+  AND Num = prev2;
