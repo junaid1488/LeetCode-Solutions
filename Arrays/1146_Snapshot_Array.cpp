0238a79199af09d725752d34a8b8fb702213932a
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-06 15:06:15 +0530

    Refactor get method in SnapshotArray class
    Refactor get method to improve readability and performance.

--- Arrays/1146_Snapshot_Array.cpp
+++ Arrays/1146_Snapshot_Array.cpp
@@ -4,12 +4,7 @@ class SnapshotArray {
 
 
   
-    int get(int index, int snap_id) {
-        int idx = upper_bound(
-            updates[index].begin(),
-            updates[index].end(),
-            make_pair(snap_id, INT_MAX)
-        ) - updates[index].begin();
+    
 
         if (idx == 0) return 0;
 
