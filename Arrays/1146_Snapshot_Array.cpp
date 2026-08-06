f7c5e53b69541e1a41dc19ca540862e7c32f0c9d
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-06 16:00:46 +0530

    Remove get method from SnapshotArray class
    Remove unused get method implementation.

--- Arrays/1146_Snapshot_Array.cpp
+++ Arrays/1146_Snapshot_Array.cpp
@@ -9,14 +9,6 @@ public:
     }
 
 
-    
-
-    int get(int index, int snap_id) {
-        int idx = upper_bound(
-            updates[index].begin(),
-            updates[index].end(),
-            make_pair(snap_id, INT_MAX)
-        ) - updates[index].begin();
 
         if (idx == 0) return 0;
 
