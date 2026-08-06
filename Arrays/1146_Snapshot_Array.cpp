b6f213395249fea9b1bf35d1e5b685cd7f3b22c6
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-06 16:01:09 +0530

    Update 1146_Snapshot_Array.cpp

--- Arrays/1146_Snapshot_Array.cpp
+++ Arrays/1146_Snapshot_Array.cpp
@@ -1,11 +1,2 @@
-class SnapshotArray {
-    vector<vector<pair<int, int>>> updates;
-    int curSnap;
-
-public:
-    SnapshotArray(int length) {
-        updates.resize(length);
-        curSnap = 0;
-    }
 
 
