369411c3387922f629c8e369dd35df1ce631c004
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-07 19:10:00 +0530

    Simplify update logic in set method
    Refactor set method to simplify update logic.

--- Arrays/1146_Snapshot_Array.cpp
+++ Arrays/1146_Snapshot_Array.cpp
@@ -9,8 +9,3 @@ public:
     }
 
     void set(int index, int val) {
-        if (!updates[index].empty() && updates[index].back().first == curSnap)
-            updates[index].back().second = val;
-        else
-            updates[index].push_back({curSnap, val});
-    }
