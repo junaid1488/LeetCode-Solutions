2dac7fbf5d889c2a0eb48958d10857478ed4d81c
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-07 19:09:25 +0530

    Refactor get method in SnapshotArray
    Remove unnecessary checks and return statement in get method.

--- Arrays/1146_Snapshot_Array.cpp
+++ Arrays/1146_Snapshot_Array.cpp
@@ -27,8 +27,3 @@ public:
             make_pair(snap_id, INT_MAX)
         ) - updates[index].begin();
 
-        if (idx == 0) return 0;
-
-        return updates[index][idx - 1].second;
-    }
-};
