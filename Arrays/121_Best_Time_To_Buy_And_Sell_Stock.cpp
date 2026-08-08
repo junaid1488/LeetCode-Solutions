4aee47c6eb71924deea7fee564aa047395ad43c2
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-08 20:06:15 +0530

    Remove profit calculation loop from maxProfit function
    Removed the loop that calculates minPrice and maxProfit.

--- Arrays/121_Best_Time_To_Buy_And_Sell_Stock.cpp
+++ Arrays/121_Best_Time_To_Buy_And_Sell_Stock.cpp
@@ -4,10 +4,7 @@ public:
         int minPrice = INT_MAX;
         int maxProfit = 0;
 
-        for (int price : prices) {
-            minPrice = min(minPrice, price);
-            maxProfit = max(maxProfit, price - minPrice);
-        }
+       
 
         return maxProfit;
     }
