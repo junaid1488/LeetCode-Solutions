9ec116efa67ac465c5bd5556d03991f0dc807920
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-08 20:06:25 +0530

    Remove unnecessary variable declarations

--- Arrays/121_Best_Time_To_Buy_And_Sell_Stock.cpp
+++ Arrays/121_Best_Time_To_Buy_And_Sell_Stock.cpp
@@ -2,10 +2,6 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int minPrice = INT_MAX;
-        int maxProfit = 0;
-
-       
-
-        return maxProfit;
+   return maxProfit;
     }
 };
