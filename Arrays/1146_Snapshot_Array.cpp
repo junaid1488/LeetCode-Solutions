2408d96c2e6b1098b6d69bc6b9e8b104e027d49c
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-25 19:25:21 +0530

    Add curSnap variable to SnapshotArray class

--- Arrays/1146_Snapshot_Array.cpp
+++ Arrays/1146_Snapshot_Array.cpp
@@ -1,7 +1,3 @@
 class SnapshotArray {
     vector<vector<pair<int, int>>> updates;
     int curSnap;
-
-public:
-    void set(int index, int val) {
-        if (!updates[index].empty() && updates[index].back().first == curSnap)
