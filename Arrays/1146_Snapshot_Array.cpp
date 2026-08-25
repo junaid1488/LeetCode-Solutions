eb9f2180a37cb8c19f48b84f975d22d335791491
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-25 19:24:45 +0530

    Refactor set method and clean up get method
    Refactor set method and remove unused code in get method.

--- Arrays/1146_Snapshot_Array.cpp
+++ Arrays/1146_Snapshot_Array.cpp
@@ -8,10 +8,3 @@ public:
             updates[index].back().second = val;
         else
             updates[index].push_back({curSnap, val});
-    }  int get(int index, int snap_id) {
-        int idx = upper_bound(
-        if (idx == 0) return 0;
-
-        return updates[index][idx - 1].second;
-    }
-};
