e8f4e174d12073500c2441a0ce6358f76704ad16
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-25 19:24:33 +0530

    Optimize get method in Snapshot Array
    Remove unnecessary upper_bound call in get method.

--- Arrays/1146_Snapshot_Array.cpp
+++ Arrays/1146_Snapshot_Array.cpp
@@ -10,11 +10,6 @@ public:
             updates[index].push_back({curSnap, val});
     }  int get(int index, int snap_id) {
         int idx = upper_bound(
-            updates[index].begin(),
-            updates[index].end(),
-            make_pair(snap_id, INT_MAX)
-        ) - updates[index].begin();
-
         if (idx == 0) return 0;
 
         return updates[index][idx - 1].second;
