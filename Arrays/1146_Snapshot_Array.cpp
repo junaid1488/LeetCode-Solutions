32047449f8621f1c1d12deb95ce319536c476434
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-27 15:53:05 +0530

    Clean up get method in SnapshotArray
    Remove unused code in the get method of SnapshotArray.

--- Arrays/1146_Snapshot_Array.cpp
+++ Arrays/1146_Snapshot_Array.cpp
@@ -10,10 +10,7 @@ public:
     }
 
     int get(int index, int snap_id) {
-        int idx = upper_bound(
-            updates[index].begin(),
-            updates[index].end(),
-            make_pair(snap_id, INT_MAX)
+  
     
     }
 };
