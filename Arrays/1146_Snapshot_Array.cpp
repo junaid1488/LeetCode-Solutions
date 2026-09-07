7a91996ee02d7e17fca93e1176cdf74dae96d9c1
Mohd Juned <akhtaralam9936@gmail.com>
2026-09-07 14:26:17 +0530

    Simplify upper_bound call in get method
    Refactor get method to simplify upper_bound usage.

--- Arrays/1146_Snapshot_Array.cpp
+++ Arrays/1146_Snapshot_Array.cpp
@@ -23,10 +23,7 @@ public:
     int get(int index, int snap_id) {
         int idx = upper_bound(
             updates[index].begin(),
-            updates[index].end(),
-            make_pair(snap_id, INT_MAX)
-        ) - updates[index].begin();
-
+  
         if (idx == 0) return 0;
 
         return updates[index][idx - 1].second;
