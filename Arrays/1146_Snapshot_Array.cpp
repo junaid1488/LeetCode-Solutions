d75679ab333cbc254ca65ec726f65c30bfc23629
Mohd Juned <akhtaralam9936@gmail.com>
2026-09-07 14:27:32 +0530

    Remove incomplete get method implementation
    Removed incomplete code for the get method in SnapshotArray.

--- Arrays/1146_Snapshot_Array.cpp
+++ Arrays/1146_Snapshot_Array.cpp
@@ -4,11 +4,6 @@ class SnapshotArray {
 
 public:
     SnapshotArray(int length) 
-
-    int get(int index, int snap_id) {
-        int idx = upper_bound(
-            updates[index].begin(),
-  
         if (idx == 0) return 0;
 
         return updates[index][idx - 1].second;
