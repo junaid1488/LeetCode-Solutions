bae047dc9293571bff6ddb5b1137abff90409aa9
Mohd Juned <akhtaralam9936@gmail.com>
2026-09-08 12:15:03 +0530

    Clean up include statements in Snapshot_Array.cpp
    Removed duplicate include statements and unnecessary code.

--- Arrays/1146_Snapshot_Array.cpp
+++ Arrays/1146_Snapshot_Array.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 using namespace std;
-}#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+
 
 int main() {
     vector<int> a = {-2,1,-3,4,-1,2,1,-5,4};
