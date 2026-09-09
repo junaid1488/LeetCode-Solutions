f6ed360ca3242e159fd6f9ecf41b3fefcb326dbe
Mohd Juned <akhtaralam9936@gmail.com>
2026-09-09 16:19:23 +0530

    Clean up main function in Snapshot Array
    Removed unused variables and code related to Roman numeral conversion.

--- Arrays/1146_Snapshot_Array.cpp
+++ Arrays/1146_Snapshot_Array.cpp
@@ -14,13 +14,6 @@ int main() {
     return 0;
 }#include <iostream
 int main() {
-    string s = "MCMXCIV";
-    unordered_map<char, int> val = {
-        {'I',1}, {'V',5}, {'X',10}, {'L',50},
-        {'C',100}, {'D',500}, {'M',1000}
-    };
-
-    int ans = 0;
 
     for (int i = 0; i < s.size(); i++
     cout << "Integer: " << ans;
